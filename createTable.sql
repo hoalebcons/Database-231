ba00618499451cfa45820f00e10758c4ed2f0fc5
Le Ngoc Hoa <87190239+hoalebcons@users.noreply.github.com>
2023-11-27 21:53:18 +0700

    first

---
+++ createTable.sql
@@ -0,0 +1,190 @@
+CREATE DATABASE store;
+USE store;
+
+-- #####################Entity#######################
+
+CREATE TABLE Customer (
+    Customer_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    Sex ENUM('M', 'F', 'O') NOT NULL, -- Assuming 'M' for male, 'F' for female, 'O' for other
+    UserName VARCHAR(255) UNIQUE NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Phone VARCHAR(20) NULL,
+    Province VARCHAR(255) NULL,
+    District VARCHAR(255) NULL,
+    Ward VARCHAR(255) NULL,
+    Street VARCHAR(255) NULL
+    -- Include additional attributes and constraints as necessary.
+);
+
+CREATE TABLE Branch (
+    Branch_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Province VARCHAR(255) NOT NULL,
+    District VARCHAR(255) NOT NULL,
+    Ward VARCHAR(255) NOT NULL,
+    Street VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Employee (
+    Employee_ID INT AUTO_INCREMENT PRIMARY KEY,
+    UserName VARCHAR(255) UNIQUE NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Phone_Number VARCHAR(20) NULL,
+    DoB DATE NULL,
+    Sex ENUM('M', 'F', 'O') NOT NULL, -- Assuming 'M' for male, 'F' for female, 'O' for other
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    Date_of_Lease DATE NULL,
+    Salary DECIMAL(10, 2) NULL, -- Assuming salary will have two decimal places.
+    Job_Type VARCHAR(255) NULL,
+    Branch_ID INT NULL,
+    Manager_ID INT NULL,
+    FOREIGN KEY (Manager_ID) REFERENCES Employee(Employee_ID)
+);
+
+CREATE TABLE Promotion (
+    Promotion_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Application_Date DATE NOT NULL,
+    Expiration_Date DATE NOT NULL,
+    Application_Terms TEXT NOT NULL,
+    Maximum_Discount DECIMAL(5, 2) NOT NULL,
+    Discount_Method ENUM('PERCENTAGE', 'AMOUNT') NOT NULL
+);
+
+CREATE TABLE Delivery (
+    Delivery_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Location VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `Order` (
+    Order_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Order_Date DATETIME NOT NULL,
+    Note TEXT,
+    Status ENUM('PROCESSING', 'COMPLETED', 'CANCELLED') NOT NULL,
+    Total_Price DECIMAL(10, 2) NOT NULL,
+    Customer_ID INT,
+    Cashier_ID INT,
+    Branch_ID INT,
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
+    FOREIGN KEY (Cashier_ID) REFERENCES Employee(Employee_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Branch(Branch_ID)
+);
+
+CREATE TABLE Bill (
+    Bill_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Feedback TEXT,
+    Customer_ID INT,
+    Order_ID INT NOT NULL,
+    Bill_Date DATE NOT NULL,
+    Total_Price DECIMAL(10, 2) NOT NULL,
+    Method ENUM('CASH', 'MOMO', 'BANKING') NOT NULL,
+    FOREIGN KEY (Customer_ID) REFERENCES Customer(Customer_ID),
+    FOREIGN KEY (Order_ID) REFERENCES `Order`(Order_ID)
+);
+
+CREATE TABLE Items (
+    Item_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Current_Price DECIMAL(10, 2) NOT NULL,
+    Note TEXT
+);
+
+CREATE TABLE Beverage (
+    Beverage_ID INT AUTO_INCREMENT PRIMARY KEY,
+    BeverageType VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Food (
+    Food_ID INT AUTO_INCREMENT PRIMARY KEY,
+    FoodType VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Product (
+    Product_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    Description TEXT,
+    Photo VARCHAR(255),
+    Item_ID INT,
+    Size VARCHAR(5),
+    Food_ID INT,
+    Beverage_ID INT,
+    FOREIGN KEY (Food_ID) REFERENCES Food(Food_ID),
+    FOREIGN KEY (Beverage_ID) REFERENCES Beverage(Beverage_ID),
+    FOREIGN KEY (Item_ID) REFERENCES Items(Item_ID)
+);
+
+CREATE TABLE Combo (
+    Combo_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Description TEXT,
+    Price DECIMAL(10, 2) NOT NULL,
+    Photo VARCHAR(255),
+    Item_ID INT,
+    FOREIGN KEY (Item_ID) REFERENCES Items(Item_ID)
+);
+
+-- #####################Relationship#######################
+
+CREATE TABLE Transport (
+    Bill_ID INT,
+    Delivery_ID INT,
+    Delivery_Fee DECIMAL(10, 2),
+    Delivery_Date DATE,
+    Shipper_Name VARCHAR(255),
+    Shipper_Phone VARCHAR(20),
+    Shipper_Vehicle VARCHAR(255),
+    Shipper_VehicleID VARCHAR(255),
+    PRIMARY KEY (Bill_ID, Delivery_ID),
+    FOREIGN KEY (Bill_ID) REFERENCES Bill(Bill_ID),
+    FOREIGN KEY (Delivery_ID) REFERENCES Delivery(Delivery_ID)
+);
+
+CREATE TABLE Event (
+    Branch_ID INT,
+    Promotion_ID INT,
+    PRIMARY KEY (Branch_ID, Promotion_ID),
+    FOREIGN KEY (Branch_ID) REFERENCES Branch(Branch_ID),
+    FOREIGN KEY (Promotion_ID) REFERENCES Promotion(Promotion_ID)
+);
+
+CREATE TABLE Discount (
+    Bill_ID INT,
+    Promotion_ID INT,
+    Discount_Price DECIMAL(10, 2),
+    Discount_Percent DECIMAL(5, 2),
+    PRIMARY KEY (Bill_ID, Promotion_ID),
+    FOREIGN KEY (Bill_ID) REFERENCES Bill(Bill_ID),
+    FOREIGN KEY (Promotion_ID) REFERENCES Promotion(Promotion_ID)
+);
+
+CREATE TABLE Order_Line (
+    Order_ID INT,
+    Item_ID INT,
+    Quantity INT NOT NULL,
+    Total_Price DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (Order_ID, Item_ID),
+    FOREIGN KEY (Order_ID) REFERENCES `Order`(Order_ID),
+    FOREIGN KEY (Item_ID) REFERENCES Items(Item_ID)
+);
+
+CREATE TABLE Combo_List (
+    Combo_ID INT,
+    Product_ID INT,
+    Quantity INT NOT NULL,
+    PRIMARY KEY (Combo_ID, Product_ID),
+    FOREIGN KEY (Combo_ID) REFERENCES Combo(Combo_ID),
+    FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID)
+);
+
+
+
+
+
+
+
+
+
---
+++ insert.sql
@@ -0,0 +1,158 @@
+INSERT INTO Customer (Name, Email, Sex, UserName, Password, Phone, Province, District, Ward, Street)
+VALUES
+('Nguyen Van A', 'nguyenvana@example.com', 'M', 'nguyenvan_a', 'password123', '0123456789', 'Ho Chi Minh', 'District 1', 'Ward 1', 'Street 1'),
+('Tran Thi B', 'tranthib@example.com', 'F', 'tranthi_b', 'password123', '0234567891', 'Ho Chi Minh', 'District 2', 'Ward 2', 'Street 2'),
+('Le Van C', 'levanc@example.com', 'M', 'levan_c', 'password123', '0345678912', 'Ho Chi Minh', 'District 3', 'Ward 3', 'Street 3'),
+('Pham Thi D', 'phamthid@example.com', 'F', 'phamthi_d', 'password123', '0456789123', 'Ho Chi Minh', 'District 4', 'Ward 4', 'Street 4'),
+('Hoang Van E', 'hoangvane@example.com', 'M', 'hoangvan_e', 'password123', '0567891234', 'Ho Chi Minh', 'District 5', 'Ward 5', 'Street 5'),
+('Nguyen Thi F', 'nguyenthif@example.com', 'F', 'nguyenthi_f', 'password123', '0678912345', 'Ho Chi Minh', 'District 6', 'Ward 6', 'Street 6'),
+('Tran Van G', 'tranvang@example.com', 'M', 'tranvan_g', 'password123', '0789123456', 'Ho Chi Minh', 'District 7', 'Ward 7', 'Street 7'),
+('Le Thi H', 'lethih@example.com', 'F', 'lethi_h', 'password123', '0891234567', 'Ho Chi Minh', 'District 8', 'Ward 8', 'Street 8'),
+('Pham Van I', 'phamvani@example.com', 'M', 'phamvan_i', 'password123', '0912345678', 'Ho Chi Minh', 'District 9', 'Ward 9', 'Street 9'),
+('Hoang Thi J', 'hoangthij@example.com', 'F', 'hoangthi_j', 'password123', '0123456780', 'Ho Chi Minh', 'District 10', 'Ward 10', 'Street 10'),
+('Nguyen Van K', 'nguyenvank@example.com', 'M', 'nguyenvan_k', 'password123', '0213456789', 'Ho Chi Minh', 'District 11', 'Ward 11', 'Street 11'),
+('Tran Thi L', 'tranthil@example.com', 'F', 'tranthi_l', 'password123', '0324567891', 'Ho Chi Minh', 'District 12', 'Ward 12', 'Street 12'),
+('Le Van M', 'levanm@example.com', 'M', 'levan_m', 'password123', '0435678912', 'Ho Chi Minh', 'Go Vap', 'Ward 13', 'Street 13'),
+('Pham Thi N', 'phamthin@example.com', 'F', 'phamthi_n', 'password123', '0546789123', 'Ho Chi Minh', 'Tan Binh', 'Ward 14', 'Street 14'),
+('Hoang Van O', 'hoangvano@example.com', 'M', 'hoangvan_o', 'password123', '0657891234', 'Ho Chi Minh', 'Tan Phu', 'Ward 15', 'Street 15'),
+('Nguyen Thi P', 'nguyenthip@example.com', 'F', 'nguyenthi_p', 'password123', '0768912345', 'Ho Chi Minh', 'Binh Thanh', 'Ward 16', 'Street 16'),
+('Tran Van Q', 'tranvanq@example.com', 'M', 'tranvan_q', 'password123', '0879123456', 'Ho Chi Minh', 'Phu Nhuan', 'Ward 17', 'Street 17'),
+('Le Thi R', 'lethir@example.com', 'F', 'lethi_r', 'password123', '0981234567', 'Ho Chi Minh', 'District 3', 'Ward 18', 'Street 18'),
+('Pham Van S', 'phamvans@example.com', 'M', 'phamvan_s', 'password123', '0923456789', 'Ho Chi Minh', 'District 1', 'Ward 19', 'Street 19'),
+('Hoang Thi T', 'hoangthit@example.com', 'F', 'hoangthi_t', 'password123', '0123456790', 'Ho Chi Minh', 'District 2', 'Ward 20', 'Street 20'),
+('Nguyen Van U', 'nguyenvanu@example.com', 'M', 'nguyenvan_u', 'password123', '0213456798', 'Ho Chi Minh', 'District 4', 'Ward 21', 'Street 21'),
+('Tran Thi V', 'tranthiv@example.com', 'F', 'tranthi_v', 'password123', '0324567892', 'Ho Chi Minh', 'District 5', 'Ward 22', 'Street 22'),
+('Le Van W', 'levanw@example.com', 'M', 'levan_w', 'password123', '0435678913', 'Ho Chi Minh', 'District 7', 'Ward 23', 'Street 23'),
+('Pham Thi X', 'phamthix@example.com', 'F', 'phamthi_x', 'password123', '0546789124', 'Ho Chi Minh', 'District 8', 'Ward 24', 'Street 24'),
+('Hoang Van Y', 'hoangvany@example.com', 'M', 'hoangvan_y', 'password123', '0657891235', 'Ho Chi Minh', 'District 9', 'Ward 25', 'Street 25'),
+('Nguyen Thi Z', 'nguyenthiz@example.com', 'F', 'nguyenthi_z', 'password123', '0768912346', 'Ho Chi Minh', 'District 10', 'Ward 26', 'Street 26'),
+('Tran Van AA', 'tranvanaa@example.com', 'M', 'tranvan_aa', 'password123', '0879123457', 'Ho Chi Minh', 'District 11', 'Ward 27', 'Street 27'),
+('Le Thi BB', 'lethibb@example.com', 'F', 'lethi_bb', 'password123', '0981234568', 'Ho Chi Minh', 'District 12', 'Ward 28', 'Street 28'),
+('Pham Van CC', 'phamvancc@example.com', 'M', 'phamvan_cc', 'password123', '0923456780', 'Ho Chi Minh', 'Go Vap', 'Ward 29', 'Street 29'),
+('Hoang Thi DD', 'hoangthidd@example.com', 'F', 'hoangthi_dd', 'password123', '0123456709', 'Ho Chi Minh', 'Tan Binh', 'Ward 30', 'Street 30');
+
+
+INSERT INTO Branch (Name, Province, District, Ward, Street)
+VALUES
+('Chi Nhánh 1', 'Ho Chi Minh', 'District 1', 'Ward 1', 'Street 1'),
+('Chi Nhánh 2', 'Ho Chi Minh', 'District 2', 'Ward 2', 'Street 2'),
+('Chi Nhánh 3', 'Ho Chi Minh', 'District 3', 'Ward 3', 'Street 3'),
+('Chi Nhánh 4', 'Ho Chi Minh', 'District 4', 'Ward 4', 'Street 4'),
+('Chi Nhánh 5', 'Ho Chi Minh', 'District 5', 'Ward 5', 'Street 5');
+
+INSERT INTO Employee (UserName, Password, Name, Phone_Number, DoB, Sex, Email, Date_of_Lease, Salary, Job_Type, Branch_ID, Manager_ID)
+VALUES
+('user_1', 'pass123', 'Nguyen Van A', '0123456789', '1981-06-15', 'M', 'nguyenvana@example.com', '2023-09-03', 3000000, 'Chef', 1, NULL),
+('user_2', 'pass123', 'Tran Thi B', '0123456788', '1982-07-20', 'F', 'tranthib@example.com', '2023-09-04', 5000000, 'Cashier', 1, NULL),
+('user_3', 'pass123', 'Le Van C', '0123456787', '1983-08-25', 'M', 'levanc@example.com', '2023-09-05', 7000000, 'Chef', 2, NULL),
+('user_4', 'pass123', 'Pham Thi D', '0123456786', '1984-09-30', 'F', 'phamthid@example.com', '2023-09-06', 3000000, 'Cashier', 2, NULL),
+('user_5', 'pass123', 'Hoang Van E', '0123456785', '1985-11-05', 'M', 'hoangvane@example.com', '2023-09-07', 5000000, 'Chef', 3, NULL),
+('user_6', 'pass123', 'Nguyen Thi F', '0123456784', '1980-12-10', 'F', 'nguyenthif@example.com', '2023-09-08', 7000000, 'Cashier', 3, NULL),
+('user_7', 'pass123', 'Tran Van G', '0123456783', '1981-01-15', 'M', 'tranvang@example.com', '2023-09-09', 3000000, 'Chef', 4, NULL),
+('user_8', 'pass123', 'Le Thi H', '0123456782', '1982-02-20', 'F', 'lethih@example.com', '2023-09-10', 5000000, 'Cashier', 4, NULL),
+('user_9', 'pass123', 'Pham Van I', '0123456781', '1983-03-27', 'M', 'phamvani@example.com', '2023-09-11', 7000000, 'Chef', 5, NULL),
+('user_10', 'pass123', 'Hoang Thi J', '0123456780', '1984-04-02', 'F', 'hoangthij@example.com', '2023-09-12', 3000000, 'Cashier', 5, NULL);
+
+INSERT INTO Promotion (Name, Application_Date, Expiration_Date, Application_Terms, Maximum_Discount, Discount_Method)
+VALUES
+('Khuyến mãi', '2023-06-01', '2023-08-31', 'Áp dụng cho tất cả khách hàng', 10.00, 'PERCENTAGE'),
+('Giảm giá', '2023-09-01', '2023-09-30', 'Áp dụng cho đơn hàng trên 500,000 VND', 50.00, 'AMOUNT'),
+('Lần đầu', '2023-10-01', '2023-10-31', 'Chỉ áp dụng cho khách hàng mua lần đầu', 15.00, 'PERCENTAGE'),
+('Ưu đãi', '2023-12-01', '2023-12-25', 'Áp dụng cho mọi đơn hàng', 100.00, 'AMOUNT');
+
+INSERT INTO Delivery (Name, Location)
+VALUES
+('Giao hàng Nhanh', 'Ho Chi Minh - District 1'),
+('Chuyển phát Siêu Tốc', 'Ho Chi Minh - District 3'),
+('Dịch vụ Giao hàng Tiết kiệm', 'Ho Chi Minh - District 5');
+
+INSERT INTO Event (Branch_ID, Promotion_ID)
+VALUES
+(1, 2),
+(1, 3),
+(3, 4),
+(5, 3);
+
+INSERT INTO Items (Item_ID, Current_Price,Note)
+VALUES
+(1, 30000,'Ghi chú 1'),
+(2, 40000,'Ghi chú 2'),
+(3, 30000,'Ghi chú 3'),
+(4, 40000,'Ghi chú 4'),
+(5, 30000,'Ghi chú 5'),
+(6, 40000,'Ghi chú 6'),
+(7, 30000,'Ghi chú 7'),
+(8, 40000,'Ghi chú 8'),
+(9, 50000,'Ghi chú 8');
+
+INSERT INTO Food (FoodType)
+VALUES
+('Đồ ăn');
+
+INSERT INTO Beverage (BeverageType)
+VALUES
+('Thức uống');
+
+INSERT INTO Product (Name, Price, Description, Photo, Size, Item_ID,Food_ID,Beverage_ID)
+VALUES
+('Cà phê', 30000.00, 'Cà phê phin truyền thống', 'ca_phe_vn.jpg','M',1,NULL,1),
+('Cà phê', 40000.00, 'Cà phê phin truyền thống', 'ca_phe_vn.jpg','L',2,NULL,1),
+('Trà Sữa', 30000.00, 'Trà sữa trân châu đường đen', 'tra_sua.jpg','M',3,NULL,1),
+('Trà Sữa', 40000.00, 'Trà sữa trân châu đường đen', 'tra_sua.jpg','L',4,NULL,1),
+('Bánh mì', 30000.00, 'Bánh mì chà bông', 'banh_mi.jpg','M',5,1,NULL),
+('Bánh mì', 40000.00, 'Bánh mì chà bông', 'banh_mi.jpg','L',6,1,NULL),
+('Bánh kem', 30000.00, 'Bánh kem Tiramisu', 'banhkem.jpg','M',7,1,NULL),
+('Bánh kem', 40000.00, 'Bánh kem Tiramisu', 'banhkem.jpg','L',8,1,NULL);
+
+INSERT INTO Combo (Name, Description, Price, Photo, Item_ID)
+VALUES
+('Combo Sáng','Cà phê và bánh mì',50000.00,'combosang.jpg',9);
+
+INSERT INTO Combo_List (Combo_ID, Product_ID, Quantity)
+VALUES
+(1, 1, 1),
+(1, 5, 1);
+
+INSERT INTO `Order` (Order_Date, Note,Total_Price,Status, Customer_ID, Cashier_ID, Branch_ID)
+VALUES
+('2023-11-21', 'Ghi chú mẫu 1',120000,'PROCESSING', 1, 2, 1),
+('2023-11-23', 'Ghi chú mẫu 2',160000,'COMPLETED', 2, 4, 2),
+('2023-11-25', 'Ghi chú mẫu 3',80000,'CANCELLED', 3, 6, 3),
+('2023-11-27', 'Ghi chú mẫu 4',30000,'COMPLETED', 4, 8, 4),
+('2023-11-29', 'Ghi chú mẫu 5',40000,'PROCESSING', 5, 10, 5);
+
+INSERT INTO Order_Line (Order_ID, Item_ID, Quantity, Total_Price)
+VALUES
+(1, 2, 2, 80000),
+(1, 4, 1, 40000),
+(2, 6, 1, 50000),
+(2, 1, 1, 30000),
+(2, 3, 2, 60000),
+(4, 3, 1, 30000),
+(5, 4, 1, 40000);
+
+
+INSERT INTO Bill (Feedback, Customer_ID, Order_ID, Total_Price, Method, Bill_Date)
+VALUES
+('Rất hài lòng', 1, 1, 100000, 'CASH', '2023-11-22'),
+('Tốt, nhưng cần cải thiện', 2, 2, 150000, 'MOMO', '2023-11-24'),
+('Tốt, nhưng cần cải thiện', 4, 4, 30000, 'MOMO', '2023-11-28'),
+('Bình thường', 5, 5, 40000, 'BANKING', '2023-11-30');
+
+INSERT INTO Discount (Bill_ID, Promotion_ID, Discount_Price, Discount_Percent)
+VALUES
+(1, 1, 20000, 10.00),
+(2, 3, 10000, 15.00);
+
+
+INSERT INTO Transport (Bill_ID, Delivery_ID, Delivery_Fee, Delivery_Date, Shipper_Name, Shipper_Phone, Shipper_Vehicle, Shipper_VehicleID)
+VALUES
+(1, 1, 20000.00, '2023-11-28', 'Nguyễn Văn A', '0123456789', 'Xe máy', '59A1-123.45'),
+(2, 1, 30000.00, '2023-11-29', 'Lê Thị B', '0123456790', 'Xe máy', '59C2-678.90'),
+(3, 2, 30000.00, '2023-11-30', 'Trần Văn C', '0123456791', 'Xe máy', '59A3-234.56'),
+(4, 3, 20000.00, '2023-12-01', 'Phạm Thị D', '0123456792', 'Xe máy', '59D4-789.01');
+
+
+
+
+
